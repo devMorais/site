16f2c46c8d591550f995fade6b4f79d3f4b0d974
Fernando Morais <fernando2015.aguiar@gmail.com>
2024-04-12 03:20:02 -0300

    instalação composer

--- index.php
+++ index.php
@@ -1,16 +1,7 @@
 <?php
 
 //Arquivo index responsável pela inicialização do sistema
-require_once 'sistema/configuracao.php';
-include_once './sistema/Nucleo/Helpers.php';
-include './sistema/Nucleo/Mensagem.php';
-include './sistema/Nucleo/Controlador.php';
+require 'vendor/autoload.php';
 
-use sistema\Nucleo\Controlador;
-
-$controlador = new Controlador();
-echo '<hr>';
-var_dump($controlador);
-
-
+echo sistema\Nucleo\Helpers::saudacao();
 
