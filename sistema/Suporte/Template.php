7a6fcdb39b39f5323d0b2792e9d1c05bb6bb0388
Fernando Morais <fernando2015.aguiar@gmail.com>
2024-04-12 22:18:33 -0300

    Construção dos arquivos CSS e JS com bootstrap e implentação junto ao twig

--- sistema/Suporte/Template.php
+++ sistema/Suporte/Template.php
@@ -2,6 +2,9 @@
 
 namespace sistema\Suporte;
 
+use Twig\Lexer;
+use sistema\Nucleo\Helpers;
+
 class Template
 {
 
@@ -11,10 +14,35 @@ class Template
     {
         $loader = new \Twig\Loader\FilesystemLoader($diretorio);
         $this->twig = new \Twig\Environment($loader);
+        $lexer = new Lexer($this->twig, array(
+            $this->helpers()
+        ));
+        $this->twig->setLexer($lexer);
     }
 
-    public function renderizar(string $view, array $dados)
+    public function renderizar(string $view, array $dados): string
     {
         return $this->twig->render($view, $dados);
+    }
+
+    private function helpers(): void
+    {
+        array(
+            $this->twig->addFunction(
+                    new \Twig\TwigFunction('url', function (string $url = null) {
+                                return Helpers::url($url);
+                            })
+            ),
+            $this->twig->addFunction(
+                    new \Twig\TwigFunction('saudacao', function () {
+                                return Helpers::saudacao();
+                            })
+            ),
+            $this->twig->addFunction(
+                    new \Twig\TwigFunction('resumirTexto', function (string $texto, int $limite) {
+                                return Helpers::resumirTexto($texto, $limite);
+                            })
+            ),
+        );
     }
 }
